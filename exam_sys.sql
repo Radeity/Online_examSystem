5177554ec04bc5626da66795fb0c320f4d72b408
Radeity <45198818+Radeity@users.noreply.github.com>
2020-03-31 23:01:25 +0800

    增加删除更新级联

--- exam_sys.sql
+++ exam_sys.sql
@@ -1,4 +1,3 @@
-
 create database if not exists examsys;
 use examsys;
 DROP TABLE IF EXISTS student;
@@ -22,7 +21,7 @@ CREATE TABLE admin
     admin_pwd  varchar(50) DEFAULT NULL COMMENT '密码',
     PRIMARY KEY (admin_id)
 );
-insert into admin values(1,4008823,'彭湘玲','123456');
+insert into admin values(4008823,'彭湘玲','123456');
 
 
 DROP TABLE IF EXISTS teacher;
@@ -36,6 +35,18 @@ CREATE TABLE teacher
 );
 insert into teacher values(1,101000,'陈德华','111111');
 insert into teacher values(2,101001,'丁祥武','222222');
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user
+(
+    id       int(10) not null AUTO_INCREMENT,
+    username varchar(20)  DEFAULT NULL COMMENT '号码',
+    name     varchar(20)  DEFAULT NULL COMMENT '姓名',
+    role     varchar(255) DEFAULT NULL COMMENT '角色',
+    password varchar(255)  DEFAULT NULL COMMENT '密码',
+    PRIMARY KEY (id)
+);
+
 
 DROP TABLE IF EXISTS classinfo;
 CREATE TABLE classinfo
@@ -65,8 +76,8 @@ CREATE TABLE SC
     student_id int(10),
     class_id   int(10),
     PRIMARY KEY (id),
-    FOREIGN KEY (student_id) REFERENCES student (student_id),
-    FOREIGN KEY (class_id) REFERENCES classinfo (class_id)
+    FOREIGN KEY (student_id) REFERENCES student (student_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (class_id) REFERENCES classinfo (class_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 insert into SC values(1,1,1);
 insert into SC values(2,2,1);
@@ -79,7 +90,7 @@ CREATE TABLE TC
     subject_id int(10),
     class_id   int(10),
     PRIMARY KEY (id),
-    FOREIGN KEY (teacher_id) REFERENCES teacher (teacher_id),
+    FOREIGN KEY (teacher_id) REFERENCES teacher (teacher_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (subject_id) REFERENCES subject (subject_id),
     FOREIGN KEY (class_id) REFERENCES classinfo (class_id)
 );
@@ -94,6 +105,8 @@ CREATE TABLE point
     PRIMARY KEY (point_id),
     FOREIGN KEY (subject_id) REFERENCES subject (subject_id)
 );
+alter table TC add constraint point_ibfk_11 FOREIGN KEY (subject_id) REFERENCES subject (subject_id) on delete cascade ON UPDATE CASCADE;
+
 insert into point values(1,'数据模型',1);
 
 DROP TABLE IF EXISTS question;
@@ -107,8 +120,8 @@ CREATE TABLE question
     subject_id      int(10),
     point_id        int(10),
     PRIMARY KEY (question_id),
-    FOREIGN KEY (point_id) REFERENCES point (point_id),
-    FOREIGN KEY (subject_id) REFERENCES subject (subject_id)
+    FOREIGN KEY (point_id) REFERENCES point (point_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (subject_id) REFERENCES subject (subject_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 insert into question values(1,'数据模型的组成要素有?、数据操作、数据的完整性约束条件。',2,'','数据结构',1,1);
 
@@ -130,8 +143,8 @@ CREATE TABLE compose
     paper_id   int(10),
     teacher_id int(10),
     PRIMARY KEY (compose_id),
-    FOREIGN KEY (paper_id) REFERENCES paper (paper_id),
-    FOREIGN KEY (teacher_id) REFERENCES teacher (teacher_id)
+    FOREIGN KEY (paper_id) REFERENCES paper (paper_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (teacher_id) REFERENCES teacher (teacher_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 insert into compose values(1,1,1);
 insert into compose values(2,1,2);
@@ -152,8 +165,8 @@ CREATE TABLE exam
     deadline     datetime COMMENT '截止日期',
     last_time    int(10)     DEFAULT NULL COMMENT '考试时长',
     PRIMARY KEY (exam_id),
-    FOREIGN KEY (paper_id) REFERENCES paper (paper_id),
-    FOREIGN KEY (subject_id) REFERENCES subject (subject_id),
+    FOREIGN KEY (paper_id) REFERENCES paper (paper_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (subject_id) REFERENCES subject (subject_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 insert into exam values(1,'数据库期末考试',1,1,0,0,0,0,1,100,'2017-03-20 23:59:59',120);
 
@@ -168,22 +181,10 @@ CREATE TABLE pdetail
     question_name varchar(500),
     cor_answer    varchar(500),
     primary key (detail_id),
-    FOREIGN KEY (paper_id) REFERENCES paper (paper_id),
-    FOREIGN KEY (question_id) REFERENCES question (question_id)
+    FOREIGN KEY (paper_id) REFERENCES paper (paper_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (question_id) REFERENCES question (question_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 insert into pdetail values(1,1,1,2,'','数据模型的组成要素有?、数据操作、数据的完整性约束条件。','数据结构');
-
-DROP TABLE IF EXISTS quema;
-CREATE TABLE quema
-(
-    quema_id   int(10) not null AUTO_INCREMENT,
-    subject_id int(10),
-    teacher_id int(10),
-    primary key (quema_id),
-    FOREIGN KEY (subject_id) REFERENCES subject (subject_id),
-    FOREIGN KEY (teacher_id) REFERENCES teacher (teacher_id)
-);
-insert into quema values(1,1,1);
 
 DROP TABLE IF EXISTS answer;
 CREATE TABLE answer
@@ -195,9 +196,9 @@ CREATE TABLE answer
     stu_answer  varchar(500) DEFAULT NULL COMMENT '学生答案',
     answer_check varchar(1)  DEFAULT NULL COMMENT '正误判断',
     primary key (answer_id),
-    FOREIGN KEY (student_id) REFERENCES student (student_id),
-    FOREIGN KEY (paper_id) REFERENCES paper (paper_id),
-    FOREIGN KEY (question_id) REFERENCES question (question_id)
+    FOREIGN KEY (student_id) REFERENCES student (student_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (paper_id) REFERENCES paper (paper_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (question_id) REFERENCES question (question_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 insert into answer values(1,1,1,1,'数据结构','F');
 
@@ -213,10 +214,10 @@ CREATE TABLE score
     paper_id   int(10),
     submit     datetime   DEFAULT NULL COMMENT '提交时间',
     primary key (score_id),
-    FOREIGN KEY (student_id) REFERENCES student (student_id),
-    FOREIGN KEY (subject_id) REFERENCES subject (subject_id),
-    FOREIGN KEY (class_id) REFERENCES classinfo (class_id),
-    FOREIGN KEY (paper_id) REFERENCES paper (paper_id)
+    FOREIGN KEY (student_id) REFERENCES student (student_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (subject_id) REFERENCES subject (subject_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (class_id) REFERENCES classinfo (class_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (paper_id) REFERENCES paper (paper_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 insert into score values(1,100,1,1,1,1,'2017-03-20 15:20:16');
 
@@ -227,8 +228,8 @@ CREATE TABLE ec
     exam_id     int(10),
     class_id    int(10),
     primary key (id),
-    FOREIGN KEY (exam_id) REFERENCES  exam(exam_id),
-    FOREIGN KEY (class_id) REFERENCES classinfo (class_id)
+    FOREIGN KEY (exam_id) REFERENCES  exam(exam_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (class_id) REFERENCES classinfo (class_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 set @@global.auto_increment_increment = 1;
